e53a4d302bb341c5150a46da44037d932c60b94a
LiuChao <571904459@qq.com>
2019-04-13 18:57:52 +0800

    完善部分功能，上传dev

---
+++ src/main/java/com/blog/service/CommentService.java
@@ -0,0 +1,35 @@
+package com.blog.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.blog.entity.Comment;
+
+/**
+ * 评论Service接口
+ * @author Administrator
+ *
+ */
+public interface CommentService {
+
+	/**
+	 * 查询用户评论信息
+	 * @param map
+	 * @return
+	 */
+	public List<Comment> list(Map<String,Object> map);
+	
+	/**
+	 * 添加评论
+	 * @param comment
+	 * @return
+	 */
+	public int add(Comment comment);
+	
+	/**
+	 * 获取总记录数
+	 * @param map
+	 * @return
+	 */
+	public Long getTotal(Map<String,Object> map);
+}
---
+++ src/main/java/com/blog/service/impl/CommentServiceImpl.java
@@ -0,0 +1,37 @@
+package com.blog.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.blog.dao.CommentDao;
+import com.blog.entity.Comment;
+import com.blog.service.CommentService;
+
+/**
+ * 评论Service实现类
+ * @author Administrator
+ *
+ */
+@Service("commentService")
+public class CommentServiceImpl implements CommentService{
+
+	@Resource
+	private CommentDao commentDao;
+	
+	public List<Comment> list(Map<String, Object> map) {
+		return commentDao.list(map);
+	}
+
+	public int add(Comment comment) {
+		return commentDao.add(comment);
+	}
+
+	public Long getTotal(Map<String, Object> map) {
+		return commentDao.getTotal(map);
+	}
+
+}
